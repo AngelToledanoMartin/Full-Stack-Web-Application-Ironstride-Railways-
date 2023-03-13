8188305fa9a3c7ffa1d89c375044f95e70d01b6a
angel2433 <angeltjob@gmail.com>
2023-03-14 00:42:25 +0100

    Adding frontend with react

---
+++ studentfrontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Appbar from './components/appBar.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar/>
+    </div>
+  );
+}
+
+export default App;
